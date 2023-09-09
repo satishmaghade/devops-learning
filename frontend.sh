77033407d5cd79b9e2fb2085600648169e897d83
lsmaghade <leenamaghade1506@gmail.com>
2023-09-09 12:37:16 +0530

    script changes

--- frontend.sh
+++ frontend.sh
@@ -3,13 +3,12 @@ dnf install nginx -y
 systemctl enable nginx 
 systemctl start nginx 
 
+
 rm -rf /usr/share/nginx/html/* 
 
-Download the frontend content
+cp expense.conf /etc/nginx/default.d/expense.conf 
 
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip 
-
-Extract the frontend content.
 
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
