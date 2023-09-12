8d889036f3b28ce79c79d515fd7a1ae9138f6ebe
lsmaghade <leenamaghade1506@gmail.com>
2023-09-12 11:24:49 +0530

    committed

---
+++ launch-ec2.sh
@@ -0,0 +1,12 @@
+- name: start multiple instances
+  amazon.aws.ec2_instance:
+    instance_type: t3.micro
+    image_id: ami-081609eef2e3cc958
+    exact_count: 2
+    region: us-east-2
+    vpc_subnet_id: subnet-0a8d2f115f3d6043c
+    network:
+      assign_public_ip: true
+      security_group: default
+    tags:
+      foo: bar
